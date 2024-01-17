2e0d2fb19761df8d1875835c9e52d54e57c381b6
Banjirsultana90 <banjirsultana15@gmail.com>
2024-01-17 13:38:47 +0600

    new pushed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
            <h1>Hello translators</h1> 
            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas autem voluptates, repudiandae corporis corrupti ullam suscipit nesciunt excepturi ab rem nobis eos dolorem quaerat optio earum et itaque nam? In!</p>
            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, eveniet non eos officia sequi minus, cum voluptatem libero, illo veniam dignissimos veritatis eligendi labore at dolor? Distinctio ea dignissimos tenetur?</p>
+           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat eos eveniet eligendi exercitationem fugit voluptatum ipsam vero vitae numquam atque excepturi sint voluptatem, tenetur, voluptate amet obcaecati nulla neque nostrum?</p>
 
         </div>
     );
