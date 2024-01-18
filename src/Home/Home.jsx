08a636d1df6657a165b09c5e859759af16efec37
Dibyendu Pramanik <chotuu668@gmil.com>
2024-01-18 23:50:48 +0530

    test

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,17 @@
-
-
+import Banner from "../Components/Banner/Banner";
+import NewsLetter from "../Components/NewsLetter/NewsLetter";
+import Package from "../Components/Package/Package";
+import Review from "../Components/Review/Review";
 
 const Home = () => {
-    return (
-        <div className="container mx-auto">
-           
-        </div>
-    );
+  return (
+    <div className="container mx-auto">
+      <Banner></Banner>
+      <Package></Package>
+      <Review></Review>
+      <NewsLetter></NewsLetter>
+    </div>
+  );
 };
 
 export default Home;
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
-import Package from "./Components/Package/Package";
-import Review from "./Components/Review/Review";
-import Banner from "./Components/Banner/Banner";
 
+import { Toaster } from "react-hot-toast";
 
 const Root = () => {
     return (
