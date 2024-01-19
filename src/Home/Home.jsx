aec8c1cdbae09fc0cccb7da3cece31a7f23ca42d
robiul704 <robiulislam29935@gmail.com>
2024-01-19 14:38:13 +0600

    faq part is added into landing page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../Components/Banner/Banner";
+import Faq from "../Components/Faq/Faq";
 import NewsLetter from "../Components/NewsLetter/NewsLetter";
 import Package from "../Components/Package/Package";
 import Review from "../Components/Review/Review";
@@ -10,6 +11,7 @@ const Home = () => {
       <Package></Package>
       <Review></Review>
       <NewsLetter></NewsLetter>
+      <Faq></Faq>
     </div>
   );
 };
