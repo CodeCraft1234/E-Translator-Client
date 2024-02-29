fbf74e1ebc68dafc7090b30a1f5e7ae3e34dc30f
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-02-29 23:59:13 +0600

    blog details page form reset issue solve

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -69,7 +69,6 @@ const Review = () => {
                   ? "bg-opacity-10 bg-white dark:bg-black dark:bg-opacity-10 p-8 rounded-lg shadow-md backdrop-blur-md"
                   : ""
               }`}
-              
             >
               <div className="testimonial">
                 <img
