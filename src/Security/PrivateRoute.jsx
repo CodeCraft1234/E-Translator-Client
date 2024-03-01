b9f027b05bb78076a476507912c8ae6f9d55d189
robiul704 <robiulislam29935@gmail.com>
2024-03-01 15:04:35 +0600

    blog delete and update work is done

--- src/Security/PrivateRoute.jsx
+++ src/Security/PrivateRoute.jsx
@@ -7,7 +7,11 @@ const PrivateRoute = ({children}) => {
     const {user,loading}=useContext(AuthContext)
     const location=useLocation()
     if(loading){
-       return <p>loading.....</p>
+       return <div className="flex items-center justify-center space-x-2">
+       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
+       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
+       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
+   </div>
     }
     if(user){
         return children;
