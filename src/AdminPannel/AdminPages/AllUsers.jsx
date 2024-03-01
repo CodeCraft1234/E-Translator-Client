9b04c767d4deb2b0b17b7a920cd2ec629756f6a3
Dibyendu Pramanik <chotuu668@gmil.com>
2024-03-01 19:34:07 +0530

    upload

--- src/AdminPannel/AdminPages/AllUsers.jsx
+++ src/AdminPannel/AdminPages/AllUsers.jsx
@@ -171,8 +171,8 @@ const AllUsers = () => {
   return (
     <div className="p-20 bg-gradient-to-r from-[#1e1b4b] via-indigo-800 to-[#1e1b4b] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 min-h-full lg:grid-cols-3 p-18 gap-4">
       {users.map((user) => (
-        <div key={user.id} className="user-card p-4 border border-gray-300 rounded">
-          <img src={user.photo} alt={user.name} className="user-image w-full h-32 object-cover mb-4" />
+        <div key={user.id} className="p-4 border border-gray-300 rounded">
+          <img src={user.photo} alt={user.name} className="rounded-full w-28 h-28 mx-auto object-cover mb-4" />
           <div >
             <h3 className="text-lg text-white font-semibold">{user.name}</h3>
             <p className="text-white">{user.email}</p>
