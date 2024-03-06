014eb64e11a71b99ae53d80f38883d23e24bd3e8
Banjirsultana90 <banjirsultana15@gmail.com>
2024-03-06 14:09:30 +0600

    dashboard updated

--- src/Axios/UseAxiosPublic.jsx
+++ src/Axios/UseAxiosPublic.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const AxiosPublic = axios.create({
-  baseURL: "https://e-translator-server.vercel.app",
+  // baseURL: "https://e-translator-server.vercel.app",
+  baseURL: 'http://localhost:5000',
 });
 const UseAxiosPublic = () => {
   return AxiosPublic;
