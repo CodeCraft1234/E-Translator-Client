a6ce0a46058fcab9e0491701b800e74e86b4c57a
robiul704 <robiulislam29935@gmail.com>
2024-03-06 10:46:05 +0600

    fix admin security for the admin page

--- src/Security/AdminSecurity.jsx
+++ src/Security/AdminSecurity.jsx
@@ -1,27 +1,6 @@
-// import React, { useContext } from 'react';
-// import { Navigate, useLocation } from 'react-router-dom';
-// import useAdmin from '../Components/hook/useAdmin';
-// import { AuthContext } from './AuthProvider';
 
-// const AdminSecurity = ({children}) => {
-//     const {user,loading}=useContext(AuthContext)
-//     const [isAdmin,isAdminloading]=useAdmin()
-//     const location=useLocation()
-//     if(loading||isAdminloading){
-//         return <h2 className='text-center text-2xl'>loading</h2>
-//     }
-//     if(user && isAdmin){
-//         return children
-//     } 
-//     return <Navigate state={location.pathname} to='/Login'></Navigate>
-   
-// };
 
-// export default AdminSecurity;
-
-// AdminSecurity.jsx
-
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import useAdmin from '../Components/hook/useAdmin';
 import { AuthContext } from './AuthProvider';
